83f361763271baf159ed24f2d2f2ddb68386a1c6
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-09-04 12:57:34 +0530

    Update models.py

--- banking_system/app/models.py
+++ banking_system/app/models.py
@@ -1,22 +1,21 @@
-from sqlalchemy import Column, Integer, String, Float, Enum, DateTime, ForeignKey
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Float, Enum, DateTime, ForeignKey, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 import enum
 
 Base = declarative_base()
 
-
 class AccountType(enum.Enum):
     SAVINGS = "SAVINGS"
     CHECKING = "CHECKING"
-
 
 class Account(Base):
     __tablename__ = "accounts"
 
     id = Column(Integer, primary_key=True, index=True)
-    owner = Column(String, index=True)
-    balance = Column(Float)
+    owner = Column(String, index=True, nullable=False)
+    balance = Column(Float, nullable=False)
     account_type = Column(Enum(AccountType), default=AccountType.SAVINGS)
     fee = Column(Float)
     interest_rate = Column(Float)
@@ -33,4 +32,14 @@ class Transaction(Base):
     timestamp = Column(DateTime)
 
     account = relationship("Account", back_populates="transactions")
+
+
+class InterestResponse(BaseModel):
+    account_id: int
+    interest_amount: float
+    interest_rate: float
+    balance: float
+
+
+
 
