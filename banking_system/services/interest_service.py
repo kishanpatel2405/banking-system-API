61e2aa4bdbbaf84f51ed6cb251dae5a564ef8c48
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-09-12 11:51:12 +0530

    Add files via upload

---
+++ banking_system/services/interest_service.py
@@ -0,0 +1,17 @@
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+from models.account import Account as AccountModel
+
+
+def compute_interest(db: Session, account_id: int, years: int, interest_rate: float = 0.05):
+    try:
+        account = db.query(AccountModel).filter(AccountModel.id == account_id).first()
+        if account:
+            interest = account.balance * interest_rate * years
+            total_amount = account.balance + interest
+            return {"account_id": account_id, "balance": account.balance, "interest": interest,
+                    "total_amount": total_amount}
+        else:
+            return {"error": "Account not found"}
+    except SQLAlchemyError as e:
+        return {"error": str(e)}
