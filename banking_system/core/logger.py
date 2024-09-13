adc0676a46700b1489c1ab1437301505ba09c0de
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-09-13 11:08:13 +0530

    Delete banking_system directory

--- banking_system/core/logger.py
+++
@@ -1,8 +0,0 @@
-# core/logger.py
-
-import logging
-
-# Set up the logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-logger = logging.getLogger(__name__)
