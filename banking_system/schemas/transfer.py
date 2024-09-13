dd6621d40cbc3c7d5e9380e7c4198c3310a25c19
kishan vaghasiya <145438858+kishanpatel2405@users.noreply.github.com>
2024-09-13 18:29:15 +0530

    Delete banking_system directory

--- banking_system/schemas/transfer.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Transfer(BaseModel):
-    from_account_id: int = Field(..., description="ID of the sender's account")
-    to_account_id: int = Field(..., description="ID of the receiver's account")
-    amount: float = Field(..., description="Amount to transfer")
